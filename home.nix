25241ad3c0558180c8d888c16f67359df6e31235
Eduardo Espadeiro <eduardo.espadeiro@rnl.tecnico.ulisboa.pt>
2021-12-23 13:23:21 +0000

    My current config

---
+++ home.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "eduardoespadeiro";
+  home.homeDirectory = "/Users/eduardoespadeiro";
+
+  home.packages = [
+    pkgs.bat
+    pkgs.exa
+    pkgs.git
+    pkgs.gcc
+    pkgs.neofetch
+    pkgs.tmux
+    pkgs.wget
+  ];
+
+  # Neovim
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
+    ];
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
